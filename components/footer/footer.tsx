552a39538a333db98da3c2f7ff8315b76c691e27
nazzarkevich <nazarjtk@gmail.com>
2022-05-28 00:26:40 +0100

    add maintenance page, fav icons

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from 'next';
+import type { NextComponentType } from 'next';
 
-export const Footer: NextPage = () => {
+export const Footer: NextComponentType = () => {
   return (
     <footer>
       <p>Page footer</p>
