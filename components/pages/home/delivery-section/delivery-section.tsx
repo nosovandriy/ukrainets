95bd16034ea72d8fc3079607abd72c0e79dcb38e
nazzarkevich <nazzik.3@gmail.com>
2023-01-08 15:03:48 +0000

    fixed svg component

--- components/pages/home/delivery-section/delivery-section.tsx
+++ components/pages/home/delivery-section/delivery-section.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
 import style from './delivery-section.module.scss';
 
+import Image from 'next/image';
 
 export const DeliverySection: React.FC = () => {
   return (
@@ -9,7 +9,8 @@ export const DeliverySection: React.FC = () => {
         Замовляйте в Україні з доставкою у Лондон
       </h3>
       <p className={style.deliverySection__subTitle}>
-        Знаходитесь у Лондоні та хочете зробити замовлення у вашому улюбленому українському магазині? Ми Вам з цим допоможемо!
+        Знаходитесь у Лондоні та хочете зробити замовлення у вашому улюбленому
+        українському магазині? Ми Вам з цим допоможемо!
       </p>
 
       <div className={style.deliverySection__shoppingImage}>
@@ -22,13 +23,15 @@ export const DeliverySection: React.FC = () => {
 
       <div className={style.deliverySection__aboutDelivery}>
         <p className={style.deliverySection__aboutDelivery__info}>
-          Замовляйте доставку Новою Поштою з українських магазинів на нашу адресу, та ми доставимо Ваше замовлення в Лондон, прямісенько до Вас додому.
+          Замовляйте доставку Новою Поштою з українських магазинів на нашу
+          адресу, та ми доставимо Ваше замовлення в Лондон, прямісенько до Вас
+          додому.
         </p>
 
         <p className={style.deliverySection__aboutDelivery__adress}>
-          Наша адреса: м. Тернопіль, вул. Тернопільська, буд. 00, оф. 0        </p>
+          Наша адреса: м. Тернопіль, вул. Тернопільська, буд. 00, оф. 0{' '}
+        </p>
       </div>
-
     </section>
   );
 };
