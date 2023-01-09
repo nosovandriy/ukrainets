ba1b0fac5f02ccdff03032c3d866db92ff52793b
Andriy Nosov <nosovandriy@gmail.com>
2023-01-09 11:38:22 +0200

    feature/UA-12-footer add formatting contacts function

---
+++ consts/default-contacts.ts
@@ -0,0 +1,30 @@
+export const contacts = [
+  {
+    "id": 1,
+    "country": "England",
+    "title": "Англія",
+    "firstInfo": "+447738204457",
+    "secondInfo": "+447982651844",
+  },
+  {
+    "id": 2,
+    "country": "Ukraine",
+    "title": "Україна",
+    "firstInfo": "+380677144424",
+    "secondInfo": "",
+  },
+  {
+    "id": 3,
+    "country": "",
+    "title": "E-mail",
+    "firstInfo": "contact@britishpost.com.ua",
+    "secondInfo": "",
+  },
+];
+
+export const fbContact = 
+  {
+    "id": 1,
+    "title": "Facebook",
+    "info": "https://www.facebook.com/ukrainets.in.ua",
+  };
