f405197f0a5ca89333f6c15a57d78f3f5454f868
nazzarkevich <nazzik.3@gmail.com>
2023-02-12 23:42:16 +0000

    fixed fav icon and hero banner

--- components/pages/home/hero-section/hero-banner/hero-banner.tsx
+++ components/pages/home/hero-section/hero-banner/hero-banner.tsx
@@ -1,17 +1,18 @@
 import style from './hero-banner.module.scss';
 
-import Image from 'next/legacy/image';
+import Image from 'next/image';
 
 export const HeroBanner: React.FC = () => {
   return (
     <div className={style.banner}>
       <Image
         className={style.banner1}
-        src="/main-banner.png"
-        layout='fill'
+        src="/main-banner.webp"
+        fill
+        sizes="100%"
         priority
-        placeholder='blur'
-        blurDataURL={"/main-banner.png"}
+        placeholder="blur"
+        blurDataURL={'/main-banner.webp'}
         alt="Picture of delivery man with parcels. Ukraine and England flag on background"
       />
     </div>
