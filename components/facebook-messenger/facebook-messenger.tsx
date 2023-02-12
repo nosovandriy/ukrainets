7ac503b8462e0e51e45db372898a91719d7b4817
nazzarkevich <nazzik.3@gmail.com>
2023-02-12 22:54:22 +0000

    fix fb function

--- components/facebook-messenger/facebook-messenger.tsx
+++ components/facebook-messenger/facebook-messenger.tsx
@@ -1,30 +1,33 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef } from 'react';
 
 export const MessengerFbChat = () => {
   const messengerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (messengerRef.current) {
-      messengerRef.current.setAttribute("attribution_version", "biz_inbox");
-      messengerRef.current.setAttribute("attribution", "install_email");
-      messengerRef.current.setAttribute("page_id", "116713648921145");
+      messengerRef.current.setAttribute('attribution_version', 'biz_inbox');
+      messengerRef.current.setAttribute('attribution', 'install_email');
+      messengerRef.current.setAttribute('page_id', '116713648921145');
+
+      (function (d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        (js as any).src =
+          'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        (fjs as any).parentNode.insertBefore(js, fjs);
+      })(document, 'script', 'facebook-jssdk');
 
       (window as any).fbAsyncInit = function () {
         const FB = {};
-        
+
         (FB as any).init({
           xfbml: true,
-          version: 'v16.0'
+          version: 'v16.0',
         });
       };
-
-      (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        (js as any).src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        (fjs as any).parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
     }
   }, []);
 
@@ -34,4 +37,4 @@ export const MessengerFbChat = () => {
       <div className="fb-customerchat" ref={messengerRef} />
     </>
   );
-}
+};
