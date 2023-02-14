09d5ad99826ea9510642c8f314322c1f1aab1fca
Andriy Nosov <nosovandriy@gmail.com>
2023-02-15 01:21:09 +0200

    fix/og-add bug fix

--- components/open-graph/open-graph.tsx
+++ components/open-graph/open-graph.tsx
@@ -4,7 +4,6 @@ export const OpenGraph = () => {
       <meta property="og:title" content="Ukrainets - Перевезення посилок Україна-Лондон" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://www.ukrainets.in.ua" />
-      <meta property="og:image" content="https://www.ukrainets.in.ua/_next/image?url=%2Fmain-banner.webp&w=1920&q=75" />
     </>
   );
 };
