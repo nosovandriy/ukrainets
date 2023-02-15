bd2979e294df643910daf5e59302260f5aab5a41
Andriy Nosov <nosovandriy@gmail.com>
2023-02-15 13:52:26 +0200

    fix/og-add og fix, add error-boundary

--- components/open-graph/open-graph.tsx
+++ components/open-graph/open-graph.tsx
@@ -1,8 +1,16 @@
+import Head from 'next/head';
+
 export const OpenGraph = () => {
   return (
     <>
-      <meta property="og:title" content="Ukrainets - Перевезення посилок Україна-Лондон" />
-      <meta property="og:type" content="website" />
+      <Head>
+        <html lang="uk" />
+        <meta charSet="utf-8" />
+        <meta property="og:title" content="Ukrainets - Перевезення посилок Україна-Лондон" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.ukrainets.in.ua/" />
+        <meta property="og:image" content="https://www.ukrainets.in.ua/_next/image?url=%2Fmain-banner.webp&w=1920&q=75" />
+      </Head>
     </>
   );
 };
