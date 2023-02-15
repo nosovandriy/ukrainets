188e926bdb5ed38404c6e1cd4bb6e00daccdba59
Andriy Nosov <nosovandriy@gmail.com>
2023-02-15 13:58:59 +0200

    fix/og-add og commit

--- components/pages/home/home-header/home-header.tsx
+++ components/pages/home/home-header/home-header.tsx
@@ -5,11 +5,11 @@ export const HomeHeader: NextComponentType = () => {
   return (
 
     <Head>
-      <meta charSet="utf-8" />
+      {/* <meta charSet="utf-8" />
       <meta property="og:title" content="Ukrainets - Перевезення посилок Україна-Лондон" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://www.ukrainets.in.ua/" />
-      <meta property="og:image" content="https://www.ukrainets.in.ua/_next/image?url=%2Fmain-banner.webp&w=1920&q=75" />
+      <meta property="og:image" content="https://www.ukrainets.in.ua/_next/image?url=%2Fmain-banner.webp&w=1920&q=75" /> */}
 
       <title>Ukrainets - Перевезення посилок з Лондона</title>
       <meta name="description" content="Ukrainets" />
